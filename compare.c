cbd6287389f72fc4f8f48b3ea57c43a12ddab40b
okynawah <onemail04@gmail.com>
2020-05-14 02:16:58 +0200

    file cleared
    inverted the value of doublel, spacesensitive, dashsensitive
    still something to do l65 ?
    spaces

--- compare.c
+++ compare.c
@@ -36,30 +36,27 @@ int hcmp(struct params *t, char *str1, char *str2)
   hcmp_clean(*t);
   sl1 = my_strlen(str1);
   sl2 = my_strlen(str2);
-
   str1 = lower_case(str1);
   str2 = lower_case(str2);
 
   for (unsigned short i = 0, j = 0; i < sl1 || j < sl2; i++, j++) {
-
     do {
       s1 = str1[i];
       s2 = str2[j];
       statut = 0;
 
-      if (t->spacesensitive == 1) {
+      if (t->spacesensitive == 0) {
         if (hcmp_check_space(s2)) {++j; statut+=1;};
       }
-      if (t->dashsensitive == 1) {
+      if (t->dashsensitive == 0) {
         if (hcmp_check_dash(s2)) {++j; statut+=1;};
       }
-      if (t->doublel == 1)
+      if (t->doublel == 0)
       {
         if (s1 == str1[i+1] && s1 != str2[j+1]) {++i; statut+=1;};
         if (s2 == str2[j+1] && s2 != str1[i+1]) {++j; statut+=1;};
       }
       proximity += statut;
-      printf("boucle %d\n", statut);
     } while(statut != 0);
 
     if (s1 != s2)
@@ -70,16 +67,15 @@ int hcmp(struct params *t, char *str1, char *str2)
   return proximity;
 }
 
-
 int main()
 {
-  char *str1 = "hello";//default
+  char *str1 = "hello";
   char *str2 = "heLo";
   struct params *t = malloc(sizeof(struct params));
-  t->dashsensitive = 1;
-  t->spacesensitive = 1;
-  t->doublel = 1;
-
+  
+  t->dashsensitive = 0;
+  t->spacesensitive = 0;
+  t->doublel = 0;
 
   printf("%d\n", hcmp(t, str1, str2));
 
